f1b611d6295d9fd6b9f344a54da18bb5c12381d0
Mateus Almeida <mateus.belchior.98@gmail.com>
2022-07-13 16:51:55 -0300

    calculo de modificadores

--- pages/cadastro-ficha.tsx
+++ pages/cadastro-ficha.tsx
@@ -4,11 +4,14 @@ import styles from '../styles/Home.module.css'
 
 const CadastroFicha: NextPage = () => {
 
-    function calculaMod(id: String, resultado: String){
-        let atributo = parseInt(document.getElementById(id).te);
-        let valor = document.getElementById(resultado);
+    function calculaMod(id: string, resultado: string) {
+        let atributo = parseInt((document.getElementById(id) as HTMLInputElement).value);
+        let valor = document.getElementById(resultado) as HTMLInputElement;
 
-        
+        valor.value = Math.floor((-5 + (atributo / 2))).toString();
+
+
+
     }
 
     return (
@@ -24,21 +27,8 @@ const CadastroFicha: NextPage = () => {
                             <span className={styles.field_span}>Nome do Personagem</span>
                             <input className={styles.field_form} type='text'></input>
                         </label>
-                        <label className={styles.text_field} >
-                            <span className={styles.field_span}>Jogador</span>
-                            <input className={styles.field_form} type='text'></input>
-                        </label>
                     </p>
                     <p>
-                        <label className={styles.text_field} htmlFor="race" >
-                            <span className={styles.field_span}>Raça</span>
-                            <select className={styles.field_form} name="select" id="race">
-                                <option value='1'>Anão</option>
-                                <option value='2'>Elfo</option>
-                                <option value='3'>Halfling</option>
-                                <option value='4'>Humano</option>
-                            </select>
-                        </label>
                         <label className={styles.text_field} htmlFor="class" >
                             <span className={styles.field_span}>Classe</span>
                             <select className={styles.field_form} name="select" id="class">
@@ -48,12 +38,20 @@ const CadastroFicha: NextPage = () => {
                                 <option value='4'>Mago</option>
                             </select>
                         </label>
+                        <label className={styles.text_field} htmlFor="race" >
+                            <span className={styles.field_span}>Raça</span>
+                            <select className={styles.field_form} name="select" id="race">
+                                <option value='1'>Anão</option>
+                                <option value='2'>Elfo</option>
+                                <option value='3'>Halfling</option>
+                                <option value='4'>Humano</option>
+                            </select>
+                        </label>
+
                         <label className={styles.text_field} >
                             <span className={styles.field_span}>Nível</span>
                             <input className={styles.field_form} type='number' min='1' max='20' />
                         </label>
-                    </p>
-                    <p>
                         <label className={styles.text_field} htmlFor="alignment">
                             <span className={styles.field_span}>Alinhamento</span>
                             <select className={styles.field_form} name="select" id="alignment">
@@ -62,51 +60,39 @@ const CadastroFicha: NextPage = () => {
                                 <option value='3'>Ordeiro</option>
                             </select>
                         </label>
-                        <label className={styles.text_field} >
-                            <span className={styles.field_span}>Caracteristicas</span>
-                            <input className={styles.field_form} />
-                        </label>
                     </p>
                     <p>
                         <label className={styles.text_field}>
                             <span className={styles.field_span}>FOR</span>
-                            <input className={styles.field_form} type='number' min='1' max='20' id="forca" />
+                            <input className={styles.field_form} type='number' min='1' max='20' id="forca" onBlur={() => calculaMod('forca', 'forDano')} />
                         </label>
                         <label className={styles.text_field}>
                             <span className={styles.field_span}>Ajuste de Ataque e Dano</span>
-                            <input className={styles.field_form} type='number' id="forDano" disabled/>
-                        </label>
-                        <label className={styles.text_field}>
-                            <span className={styles.field_span}>Carga Leve -1 Pesada -2 Máxima</span>
-                            <input className={styles.field_form} type='number' id="forCarga" disabled/>
+                            <input className={styles.field_form} type='number' id="forDano" disabled />
                         </label>
                     </p>
                     <p>
                         <label className={styles.text_field}>
                             <span className={styles.field_span}>DES</span>
-                            <input className={styles.field_form} type='number' min='1' max='20' id="destreza" />
+                            <input className={styles.field_form} type='number' min='1' max='20' id="destreza" onBlur={() => calculaMod('destreza', 'desAjuste')} />
                         </label>
                         <label className={styles.text_field}>
                             <span className={styles.field_span}>Ajustes</span>
-                            <input className={styles.field_form} type='number' id="desAjuste" disabled/>
-                        </label>
-                        <label className={styles.text_field}>
-                            <span className={styles.field_span}>Talentos de Ladinos</span>
-                            <input className={styles.field_form} type='number' id="desTalentos" disabled/>
+                            <input className={styles.field_form} type='number' id="desAjuste" disabled />
                         </label>
                     </p>
                     <p>
                         <label className={styles.text_field}>
                             <span className={styles.field_span}>CON</span>
-                            <input className={styles.field_form} type='number' min='1' max='20' id="constituicao" />
+                            <input className={styles.field_form} type='number' min='1' max='20' id="constituicao" onBlur={() => calculaMod('constituicao', 'conAjuste')} />
                         </label>
                         <label className={styles.text_field}>
                             <span className={styles.field_span}>Ajuste PV e Proteção</span>
-                            <input className={styles.field_form} type='number' id="conAjuste" disabled/>
+                            <input className={styles.field_form} type='number' id="conAjuste" disabled />
                         </label>
                         <label className={styles.text_field}>
                             <span className={styles.field_span}>% Ressurreição</span>
-                            <input className={styles.field_form} type='number' id="conRess" disabled/>
+                            <input className={styles.field_form} type='number' id="conRess" disabled />
                         </label>
                     </p>
                     <p>
@@ -116,47 +102,35 @@ const CadastroFicha: NextPage = () => {
                         </label>
                         <label className={styles.text_field}>
                             <span className={styles.field_span}>Idiomas</span>
-                            <input className={styles.field_form} type='number' id="intIdiomas" disabled/>
+                            <input className={styles.field_form} type='number' id="intIdiomas" disabled />
                         </label>
                         <label className={styles.text_field}>
                             <span className={styles.field_span}>% Aprender Magia</span>
-                            <input className={styles.field_form} type='number' id="intAprender" disabled/>
-                        </label>
-                        <label className={styles.text_field}>
-                            <span className={styles.field_span}>Magias Adicionais</span>
-                            <input className={styles.field_form} type='number' id="intMagiasAd" disabled/>
+                            <input className={styles.field_form} type='number' id="intAprender" disabled />
                         </label>
                     </p>
                     <p>
                         <label className={styles.text_field}>
                             <span className={styles.field_span}>SAB</span>
-                            <input className={styles.field_form} type='number' min='1' max='20' id="sabedoria"/>
+                            <input className={styles.field_form} type='number' min='1' max='20' id="sabedoria" onBlur={() => calculaMod('sabedoria', 'sabProtec')} />
                         </label>
                         <label className={styles.text_field}>
                             <span className={styles.field_span}>Ajuste de Proteção</span>
-                            <input className={styles.field_form} type='number' id="sabProtec" disabled/>
-                        </label>
-                        <label className={styles.text_field}>
-                            <span className={styles.field_span}>Magias Adicionais</span>
-                            <input className={styles.field_form} type='number' id="sabMagiasAd" disabled/>
+                            <input className={styles.field_form} type='number' id="sabProtec" disabled />
                         </label>
                     </p>
                     <p>
                         <label className={styles.text_field}>
                             <span className={styles.field_span}>CAR</span>
-                            <input className={styles.field_form} type='number' min='1' max='20' id="carisma"/>
+                            <input className={styles.field_form} type='number' min='1' max='20' id="carisma" />
                         </label>
                         <label className={styles.text_field}>
                             <span className={styles.field_span}>N° Seguidores</span>
-                            <input className={styles.field_form} type='number' id="carSeguidores" disabled/>
+                            <input className={styles.field_form} type='number' id="carSeguidores" disabled />
                         </label>
                         <label className={styles.text_field}>
                             <span className={styles.field_span}>Ajuste de Reação</span>
-                            <input className={styles.field_form} type='number' id="carReac" disabled/>
-                        </label>
-                        <label className={styles.text_field}>
-                            <span className={styles.field_span}>Mortos-Vivos</span>
-                            <input className={styles.field_form} type='number' id="carMortosVivos" disabled/>
+                            <input className={styles.field_form} type='number' id="carReac" disabled />
                         </label>
                     </p>
                 </form>
